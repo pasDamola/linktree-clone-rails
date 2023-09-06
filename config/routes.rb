bbc316fc6736c03a75d7042510fdaa18bd2a825e
pasDamola <oyindadavid18@gmail.com>
2023-09-06 13:55:30 +0100

    creating profile page and top level users routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboard', to: "dashboard#index"
   root 'dashboard#index'
+
+
+  get ':id', to: "dashboard#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
