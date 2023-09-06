482d817eb2e7274422cd180d3cf65ba2dabb2df9
pasDamola <oyindadavid18@gmail.com>
2023-09-06 16:14:54 +0100

    user profile avatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get 'dashboard', to: "dashboard#index"
+  get 'appearance', to: "dashboard#appearance"
   root 'dashboard#index'
 
 
